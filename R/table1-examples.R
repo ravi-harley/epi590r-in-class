04196ced5da4b95457aad39c9d53aa5bd8984bc0
Ravi Harley <ravikharley@outlook.com>
2024-08-19 15:31:53 -0400

    table

--- R/table1-examples.R
+++ R/table1-examples.R
@@ -38,36 +38,41 @@ tbl_summary(
 	missing_text = "Missing")
 
 
+Yes
+
 tbl_summary(
 	nlsy,
 	by = sex_cat,
-	include = c(sex_cat, race_eth_cat,
-							eyesight_cat, glasses, age_bir),
+	include = c(race_eth_cat, region_cat, starts_with("sleep"), income),
 	label = list(
-		race_eth_cat ~ "Race/ethnicity",
-		eyesight_cat ~ "Eyesight",
-		glasses ~ "Wears glasses",
-		age_bir ~ "Age at first birth"
-	),
-	missing_text = "Missing") |>
-	add_p(test = list(all_continuous() ~ "t.test",
-										all_categorical() ~ "chisq.test")) |>
-	add_overall(col_label = "**Total**") |>
-	bold_labels() |>
-	modify_footnote(update = everything() ~ NA) |>
-	modify_header(label = "**Variable**", p.value = "**P**")
+		race_eth_cat ~ "Race/Ethnicity",
+		region_cat ~ "Region",
+		sleep_wkdy ~ "Hours of Sleep (Weekday)",
+		sleep_wknd ~ "Hours of Sleep (Weekend)",
+		income ~ "Income"),
+	statistic = list(
+		income ~ "{p90}",
+		income ~ "{p10}",
+		starts_with("sleep") ~ "min = {min}",
+		starts_with("sleep") ~ "max = {max}"),
+	digits = list(
+		income ~ (3),
+		starts_with("sleep") ~ (1))) |>
+		add_p(test = list(all_continuous() ~ "t.test",
+											all_categorical() ~ "chisq.test")) |>
+		add_overall(col_label = "**Total**") |>
+		modify_table_styling(
+			columns = label,
+			rows = label == "Race/Ethnicity",
+			footnote = "https://www.nlsinfo.org/content/cohorts/nlsy79/topical-guide/household/race-ethnicity-immigration-data"
+		)
 
-install.packages("cardx")
-tbl_summary(
-	nlsy,
-	by =. sex_cat,
-	include = c(region_cat, race_eth_cat, income, sleep_wkdy, sleep_wknd),
-label = list(
-	race_eth_cat ~ "Race/Ethnicity",
-	region_cat ~ "Region",
-	income ~ "Income",
-	sleep_wkdy ~ "Sleep Weekday",
-	sleep_wknd ~ "Sleep Weekend" )
 
-)
+
+
+
+
+
+
+
 
